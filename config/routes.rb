39a2615cb21b5b44181cc12513b4c5b1a90107b2
Yuu <anpanmamZ3624@gmail.com>
2021-09-07 21:55:03 +0900

    ３回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
-Rails.application.routes.draw do
-    devise_for :users, controllers: {
-       registrations: 'registrations',
-    }
+Rails
+  .application
+  .routes
+  .draw do
+    devise_for :users, controllers: { registrations: 'registrations' }
 
-    root 'pages#home'
+    root 'posts#index'
 
     get '/users/:id', to: 'users#show', as: 'user'
 
-    resources :posts, only: %i(new create) do
+    resources :posts, only: %i(new create index show destroy) do
       resources :photos, only: %i(create)
+      resources :likes, only: %i(create destroy)
+      resources :comments, only: %i(create destroy)
     end
 
-    # get "/posts/new", to: "posts#new"
-    # post "/posts/", to: "posts#create"
-    # post "/posts/:post_id/photos", to: "photos#create", as: "post_photos"
-  end
+end
